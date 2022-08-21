12de90d0566f163577313bbf34ec47e71c5026e0
anthonyariasr <anthonyariasr@outlook.com>
2022-08-21 17:51:55 -0600

    Exercise 3, Practice 1 completed

--- paradigma_imperativo/practica 1/practica1.go
+++ paradigma_imperativo/practica 1/practica1.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -15,8 +16,27 @@ func CountWordsandLetters(text string, textDescrip string) string {
 
 }
 
-func RotateSequence() {
+func remove(sliceArray []string, s int) []string {
+	return append(sliceArray[:s], sliceArray[s+1:]...)
+}
 
+func RotateSequence(rotaQua int, direction bool, array *[]string) {
+
+	//true == right
+	//false == left
+
+	if direction == true {
+		for i := 0; i < rotaQua; i++ {
+			*array = append([]string{(*array)[len(*array)-1]}, *array...)
+			*array = remove(*array, len(*array)-1)
+		}
+
+	} else {
+		for i := 0; i < rotaQua; i++ {
+			*array = append(*array, (*array)[0])
+			*array = remove(*array, 0)
+		}
+	}
 }
 
 func ShoesInventory() {
@@ -26,7 +46,7 @@ func ShoesInventory() {
 func main() {
 
 	//Exercise 1
-
+	print("\n\nExercise 1: \n")
 	text := `The late twentieth century has witnessed a scientific gold rush of astonishing proportions: the 
 headlong and furious haste to commercialize genetic engineering. This enterprise has proceeded 
 so rapidly-with so little outside commentary-that its dimensions and implications are hardly 
@@ -39,7 +59,24 @@ understood at all.`
 	print(result)
 
 	//Exercise 3
-	RotateSequence()
+	print("\n\nExercise 3: \n")
+
+	//Amount of times the elements will rotate
+	numPos := 3
+
+	//true == right
+	//false == left
+	direct := true
+
+	originalArray := &[]string{"a", "b", "c", "d", "e"}
+
+	print("The original Array is: ")
+	fmt.Println(*originalArray)
+
+	RotateSequence(numPos, direct, originalArray)
+
+	fmt.Print("The array rotated " + strconv.Itoa(numPos) + " times is: ")
+	fmt.Println(*originalArray)
 
 	//Exercise 4
 	ShoesInventory()
